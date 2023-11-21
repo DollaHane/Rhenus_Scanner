eb393418f3e79d55e061ced8ff3a98224b757cbe
DollaHane <shanehubsch@gmail.com>
2023-11-21 13:28:44 +0200

    Removed loading state

--- app/Settings.js
+++ app/Settings.js
@@ -11,8 +11,6 @@ export default function Settings() {
 
   // All Files
   const [datas, setDatas] = useState([]);
-
-  const [isLoading, setIsLoading] = useState(true);
   const [info, setInfo] = useState(false);
 
   //_________________________________________________________
@@ -32,17 +30,10 @@ export default function Settings() {
           setDatas(resultSet.rows._array);
           console.log("datas:", datas);
         },
-
         (txObj, error) => console.log(error)
       );
     });
   }, [db, filePath]);
-
-  useEffect(() => {
-    if (datas.length > 0) {
-      setIsLoading(false);
-    }
-  }, [datas]);
 
   //_________________________________________________________
   // Get the latest file being used:
@@ -123,15 +114,6 @@ export default function Settings() {
   return (
     <View className="p-2 w-full h-full bg-stone-50">
 
-
-      {/* LOADING STATE */}
-      {isLoading === true && (
-        <View className="flex w-full h-full bg-white items-center rounded-lg">
-          <Text className="top-44 text-2xl">Loading..</Text>
-        </View>
-      )}
-
-
       {/* SETTINGS PAGE INFO MODAL */}
       {info === true && (
         <View className="flex w-full h-full bg-stone-50 rounded-lg">
@@ -144,37 +126,53 @@ export default function Settings() {
             </TouchableOpacity>
           </View>
           <View className="w-full h-full top-5 px-5 items-start">
-            <Text className="text-2xl font-bold text-zinc-800">User Guide:</Text>
-            <Text className="text-lg mt-5 font-semibold text-zinc-800">File Selection:</Text>
-            <Text className="mt-2 text-zinc-700 text-xs">
-              A valid .csv file is required in order to complete a scan cycle on this application. 
-              To select a file, press the <Text  className='italic font-semibold'>"Select File"</Text> button on the 
-              <Text className='italic font-semibold'>"Settings"</Text> page. A .csv file can then be selected from 
-              any directory on the device. (Moving the file will result in errors / broken file path.)  
+            <Text className="text-2xl font-bold text-zinc-800">
+              User Guide:
+            </Text>
+            <Text className="text-lg mt-5 font-semibold text-zinc-800">
+              File Selection:
             </Text>
             <Text className="mt-2 text-zinc-700 text-xs">
-              The currently selected file will be displayed under <Text className='italic font-semibold'>"Currently reading data from file name:"</Text> which 
-              represents the .csv file being used to find the locations of parts based on the part number scanned.
+              A valid .csv file is required in order to complete a scan cycle on
+              this application. To select a file, press the{" "}
+              <Text className="italic font-semibold">"Select File"</Text> button
+              on the
+              <Text className="italic font-semibold">"Settings"</Text> page. A
+              .csv file can then be selected from any directory on the device.
+              (Moving the file will result in errors / broken file path.)
             </Text>
-            <Text className="text-lg mt-5 font-semibold text-zinc-800">CSV File Format:</Text>
             <Text className="mt-2 text-zinc-700 text-xs">
-              In order to correctly process the data within the selected .csv file, all the data needs to appear 
-              within the first column of the .csv file.   
+              The currently selected file will be displayed under{" "}
+              <Text className="italic font-semibold">
+                "Currently reading data from file name:"
+              </Text>{" "}
+              which represents the .csv file being used to find the locations of
+              parts based on the part number scanned.
+            </Text>
+            <Text className="text-lg mt-5 font-semibold text-zinc-800">
+              CSV File Format:
+            </Text>
+            <Text className="mt-2 text-zinc-700 text-xs">
+              In order to correctly process the data within the selected .csv
+              file, all the data needs to appear within the first column of the
+              .csv file.
             </Text>
             <Text className="mt-2 text-zinc-700 text-xs italic font-semibold">
-              Eg.: ",1E04957H01,WCC-13-3..,WCS-4-2..,1109C..," 
+              Eg.: ",1E04957H01,WCC-13-3..,WCS-4-2..,1109C..,"
             </Text>
             <Text className="mt-2 text-zinc-700 text-xs">
-              As per the above example, after a successful scan event, the application searches for a matching the part number (first entry in the string) 
-              and then returns that part numbers warehouse locations (subsequent entries seperated by a comma).
+              As per the above example, after a successful scan event, the
+              application searches for a matching the part number (first entry
+              in the string) and then returns that part numbers warehouse
+              locations (subsequent entries seperated by a comma).
             </Text>
             <Text className="mt-5 text-zinc-700 text-base italic font-bold">
-              (Please contact the developer in the event the structure of the .csv file has been revised.)
+              (Please contact the developer in the event the structure of the
+              .csv file has been revised.)
             </Text>
           </View>
         </View>
       )}
-
 
       {/* SETTINGS PAGE UI */}
       <View className="top-5 px-5 flex-row justify-between">
